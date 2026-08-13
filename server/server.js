d3859b5ae3372588d27ad13d4c3b745c38f199bc
Reshma Showkath <reshmashowkath2002@gmail.com>
2026-08-14 00:01:04 +0530

    Prepare FixFlow for deployment

--- server/server.js
+++ server/server.js
@@ -191,6 +191,6 @@ app.get("/api/search", async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0",() => {
   console.log(` FixFlow API running on http://localhost:${PORT}`);
 });
